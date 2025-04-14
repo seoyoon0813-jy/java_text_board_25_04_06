437c9df4495ceddfc2b64a239ef6cf7d707e23bf
seoyoon0813-jy <enddldltjdbs@naver.com>
2025-04-14 22:15:10 +0900

    게시물 정보를 쉽게 다루도록 Article 클래스 도입 및 객체 생성

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -32,6 +32,15 @@ public class Main {
         }
 
         int id = ++lastArticleId;
+
+        //객체 생성 후, 객체가 가지고 잇는 변수에 데이터 저장
+        Article article = new Article();
+        article.id = id;
+        article.subject = subject;
+        article.content = comtent;
+
+        System.out.println("생성된 게시물 객체 : " + article);
+
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
 
       }
@@ -49,4 +58,10 @@ public class Main {
     sc.close();
 
   }
+}
+
+class Article {
+  int id;
+  String subject;
+  String content;
 }
