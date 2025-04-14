4e596b8e0026ceb7f37e3eb9e2e28137ad540af6
seoyoon0813-jy <enddldltjdbs@naver.com>
2025-04-14 22:41:41 +0900

    usr/article/detail 명령 입력시, 마지막에 입력된 게시물 정보 조회

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -6,6 +6,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int lastArticleId = 0;
+    Article lastArticle = null; //변수 초기화
 
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("텍스트 게시판을 시작합니다.");
@@ -14,7 +15,7 @@ public class Main {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if (cmd.equals("/user/article/write")) {
+      if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
@@ -35,16 +36,34 @@ public class Main {
 
         //객체 생성 후, 객체가 가지고 잇는 변수에 데이터 저장
         Article article = new Article(id, subject, content);
+        lastArticle = article; //해당 객체 주소값을 공유함
 
 
         System.out.println("생성된 게시물 객체 : " + article);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
 
-      } else if (cmd.equals("exit")) {
+      }
+      else if (cmd.equals("/usr/article/detail")) {
+        Article article = lastArticle;
+
+        //lastArticle는 null이 들어 있음 게시물을 등록을 안했는데 게시물 조회하면 오류남
+        //그래서 유효성 검사가 필요함 (에러 보여주면 안 되기 때문에)
+        if(article == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.println("게시물 상세보기");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.subject);
+        System.out.printf("내용 : %s\n", article.content);
+      }
+      else if (cmd.equals("exit")) {
         System.out.println("텍스트 게시판을 종료합니다.");
         break;
-      } else {
+      }
+      else {
         System.out.println("잘못 입력 된 명령어입니다");
       }
     }
