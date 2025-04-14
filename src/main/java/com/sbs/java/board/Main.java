bbda8e4e298584a2e6aa9db116718ab3f1cb0248
seoyoon0813-jy <enddldltjdbs@naver.com>
2025-04-14 21:28:13 +0900

    간단한 명령어 입력 받기

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,10 +1,22 @@
 package com.sbs.java.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("텍스트 게시판을 시작합니다.");
+
+    System.out.print("명령) ");
+    String cmd = sc.nextLine();
+
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
+
     System.out.println("텍스트 게시판을 종료합니다.");
     System.out.println("== 자바 텍스트 게시판 종료 ==");
+
+    sc.close();
   }
 }
