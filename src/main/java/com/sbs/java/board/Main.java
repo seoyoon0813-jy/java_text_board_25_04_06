f9bbbe2304264a11cb964e46a1449b0684501cfd
seoyoon0813-jy <enddldltjdbs@naver.com>
2025-04-14 21:54:54 +0900

    C:/Program Files/Git/user/article/write 명령어 처리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -13,13 +13,27 @@ public class Main {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("exit")) {
+      if (cmd.equals("/user/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+        System.out.print("제목 : ");
+        String subject = sc.nextLine();
+
+        System.out.print("내용 : ");
+        String comtent = sc.nextLine();
+
+        int id = 1;
+        System.out.printf("%d번 게시물이 등록되었습니다.", id);
+      }
+      else if (cmd.equals("exit")) {
         System.out.println("텍스트 게시판을 종료합니다.");
         break;
       }
-      System.out.printf("입력받은 명령어 : %s\n", cmd);
+      else {
+        System.out.println("잘못 입력 된 명령어입니다");
+      }
     }
-    
+
+
     System.out.println("== 자바 텍스트 게시판 종료 ==");
     sc.close();
 
