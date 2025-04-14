3368ff4f3c35e7410c8f86d296ccde2cef5203b6
seoyoon0813-jy <enddldltjdbs@naver.com>
2025-04-14 21:23:03 +0900

    시작 메시지 출력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -2,6 +2,9 @@ package com.sbs.java.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("텍스트 게시판을 시작합니다.");
+    System.out.println("텍스트 게시판을 종료합니다.");
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
   }
 }
