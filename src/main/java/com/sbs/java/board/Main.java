0acca80f2519db614dffc7f5cd46651a5b1a1099
seoyoon0813-jy <enddldltjdbs@naver.com>
2025-04-14 21:38:44 +0900

    exit를 입력받기 전까지 계속 입력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -9,14 +9,19 @@ public class Main {
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("텍스트 게시판을 시작합니다.");
 
-    System.out.print("명령) ");
-    String cmd = sc.nextLine();
+    while (true) {
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
 
-    System.out.printf("입력받은 명령어 : %s\n", cmd);
-
-    System.out.println("텍스트 게시판을 종료합니다.");
+      if(cmd.equals("exit")) {
+        System.out.println("텍스트 게시판을 종료합니다.");
+        break;
+      }
+      System.out.printf("입력받은 명령어 : %s\n", cmd);
+    }
+    
     System.out.println("== 자바 텍스트 게시판 종료 ==");
-
     sc.close();
+
   }
 }
